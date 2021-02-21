822b04414ce50fb2fd6a24f21883233b0904c5be
meaning2415 <meaning2415@gmail.com>
2021-02-21 18:40:59 +0900

    MongoDB install

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://Jimin:qwer1234@meaning.bm8ak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+    useNewUrlParser:true,useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
+}).then(() => console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port,()=> console.log(`Example app listening on port ${port}!`))
